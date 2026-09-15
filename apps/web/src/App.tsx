1e9e680d660c545f82b1343d39114e6844c14ebd
Mikhail <139760096+beluga73@users.noreply.github.com>
2026-09-16 00:33:56 +0200

    feat: set up theming

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,18 @@
 import { CONSTANT } from '@repo/ui-telemetry';
 import { Button } from '@/components/ui/button';
+import { useTheme } from './components/theme-provider';
 
 export default function App() {
+  const { theme, setTheme } = useTheme();
+
+  const handleClick = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
     <div>
       {CONSTANT}
-      <Button>button</Button>
+      <Button onClick={handleClick}>{theme}</Button>
     </div>
   );
 }
